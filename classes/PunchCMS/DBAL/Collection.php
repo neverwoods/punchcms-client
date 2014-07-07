508dc41ea419f4ca99ae00ca47a5ca617abd9364
Robin van Baalen <robin@neverwoods.com>
2014-07-06 23:36:24 -0400

    fix(collection): fixed Collection class

--- classes/PunchCMS/DBAL/Collection.php
+++ classes/PunchCMS/DBAL/Collection.php
@@ -1,58 +1,34 @@
 <?php
 
-/* General DBA Collection Class v0.3.3
- * Holds a collection of objects.
- *
- * CHANGELOG
- * version 0.3.3, 03 March 2011
- *   ADD: Added getByPropertyValue method.
- *	 ADD: Added getValueByValue method.
- *   CHG: Added comments and streamlined code.
- * version 0.3.2, 19 January 2011
- *   CHG: Enhanced the rewind method to return the collection.
- * version 0.3.1, 18 December 2009
- *   FIX: Fixed random method. Better index checks.
- * version 0.3.0, 6 December 2009
- *   FIX: Fixed merge method. Better collection checks.
- * version 0.2.9, 5 May 2009
- *   CHG: Changed paging to url based instead of request based (setCurrentPage).
- * version 0.2.8, 7 Jul 2008
- *   ADD: Added merge method.
- * version 0.2.7, 16 Jun 2008
- *   ADD: Added isFirst method.
- *   ADD: Added isLast method.
- * version 0.2.6, 18 Apr 2008
- *   FIX: Fixed setPageItems to seek to the right item.
- * version 0.2.5, 23 Jan 2008
- *   ADD: Added documentation to all methods.
- *   ADD: Added getPageByChild method.
- * version 0.2.4, 05 Nov 2007
- *   REM: Removed the orderByField method. Out of context.
- * version 0.2.3, 05 Oct 2007
- *   FIX: Fixed the inner workings of the reverse method.
- * version 0.2.2, 21 Aug 2007
- *   ADD: Added methods for paginating.
- * version 0.2.1, 02 Aug 2007
- *   ADD: Added the reverse method.
- * version 0.2.0, 16 May 2006
- *   NEW: Created class.
- */
+namespace PunchCMS\DBAL;
 
-class DBA__Collection implements Iterator {
+use Bili\Request;
+
+/**
+ * General DBAL Collection Class
+ *
+ * Holds a Collection of objects
+ *
+ * @author Felix Langfeldt <felix@neverwoods.com>
+ * @internal
+ */
+class Collection implements \Iterator
+{
 	protected $collection = array();
-	private $isSeek = FALSE;
-	private $__pageItems = 0;
-	private $__currentPage = 1;
+	private $isSeek = false;
+	private $pageItems = 0;
+	private $currentPage = 1;
 
 	/**
 	 * Constructor method
 	 *
 	 * @param array $initArray
 	 */
-	public function __construct($initArray = array()) {
-	   if (is_array($initArray)) {
-		   $this->collection = $initArray;
-	   }
+	public function __construct($initArray = array())
+	{
+	    if (is_array($initArray)) {
+	 	    $this->collection = $initArray;
+	    }
 	}
 
 	/**
@@ -61,7 +37,8 @@ class DBA__Collection implements Iterator {
 	 * @param object The object
 	 * @param boolean Add object to beginning of array or not
 	 */
-	public function addObject($value, $blnAddToBeginning = FALSE) {
+	public function addObject($value, $blnAddToBeginning = false)
+	{
 		if ($blnAddToBeginning) {
 			array_unshift($this->collection, $value);
 		} else {
@@ -74,21 +51,23 @@ class DBA__Collection implements Iterator {
 	 *
 	 * @param integer $intPosition
 	 */
-	public function seek($intPosition) {
+	public function seek($intPosition)
+	{
         if (is_numeric($intPosition) && $intPosition < count($this->collection)) {
         	reset($this->collection);
-			while($intPosition > key($this->collection)) {
+			while ($intPosition > key($this->collection)) {
 				next($this->collection);
 			}
         }
 
-		$this->isSeek = TRUE;
+		$this->isSeek = true;
 	}
 
 	/**
 	 * Pick a random child element
 	 */
-    public function random() {
+    public function random()
+    {
     	$objReturn = null;
 
     	$intIndex = rand(0, (count($this->collection) - 1));
@@ -102,14 +81,16 @@ class DBA__Collection implements Iterator {
     /**
      * Randomize the collection
      */
-    public function randomize() {
+    public function randomize()
+    {
 		shuffle($this->collection);
     }
 
     /**
      * Get an element of the collection selected by property value.
      */
-    public function getByPropertyValue($strSearchProperty, $strSearchValue) {
+    public function getByPropertyValue($strSearchProperty, $strSearchValue)
+    {
     	$objReturn = null;
 
     	foreach ($this->collection as $objElement) {
@@ -128,7 +109,8 @@ class DBA__Collection implements Iterator {
     /**
      * Get the value of a property of a specific element, selected by property value.
      */
-    public function getValueByValue($strSearchProperty, $strSearchValue, $strResultProperty = "value") {
+    public function getValueByValue($strSearchProperty, $strSearchValue, $strResultProperty = "value")
+    {
     	$strReturn = "";
 
     	$objElement = $this->getByPropertyValue($strSearchProperty, $strSearchValue);
@@ -148,7 +130,8 @@ class DBA__Collection implements Iterator {
      * @param string $strSubject
      * @param string $strOrder
      */
-	public function orderBy($strSubject, $strOrder = "asc") {
+	public function orderBy($strSubject, $strOrder = "asc")
+	{
 		for ($i = 0; $i < count($this->collection); $i++) {
 			for ($j = 0; $j < count($this->collection) - $i - 1; $j++) {
 				if ($strOrder == "asc") {
@@ -171,56 +154,64 @@ class DBA__Collection implements Iterator {
 	/**
 	 * Get the item count.
 	 */
-	public function count() {
+	public function count()
+	{
 		return count($this->collection);
 	}
 
 	/**
 	 * Get the current item from the collection.
 	 */
-    public function current() {
+    public function current()
+    {
         return current($this->collection);
     }
 
 	/**
 	 * Place the pointer one item forward and return the item.
 	 */
-    public function next() {
+    public function next()
+    {
         return next($this->collection);
     }
 
 	/**
 	 * Place the pointer one item back and return the item.
 	 */
-    public function previous() {
+    public function previous()
+    {
         return prev($this->collection);
     }
 
 	/**
 	 * Get the current position of the pointer.
 	 */
-    public function key() {
+    public function key()
+    {
         return key($this->collection);
     }
 
 	/**
 	 * Check if the pointer is at the first record.
 	 */
-    public function isFirst() {
+    public function isFirst()
+    {
         return key($this->collection) == 0;
     }
 
 	/**
 	 * Check if the pointer is at the last record.
 	 */
-    public function isLast() {
+    public function isLast()
+    {
         return key($this->collection) == (count($this->collection) - 1);
     }
 
 	/**
 	 * Merge a collection with this collection.
 	 */
-    public function merge($collection) {
+    public function merge($collection)
+    {
 		if (is_object($collection) && $collection->count() > 0) {
         	$this->collection = array_merge($this->collection, $collection->collection);
 		}
@@ -229,23 +220,25 @@ class DBA__Collection implements Iterator {
 	/**
 	 * Test if the requested item is valid.
 	 */
-    public function valid() {
-    	if ($this->__pageItems > 0) {
+    public function valid()
+    {
+    	if ($this->pageItems > 0) {
     		if ($this->key() + 1 > $this->pageEnd()) {
-    			return FALSE;
+    			return false;
     		} else {
-    			return $this->current() !== FALSE;
+    			return $this->current() !== false;
     		}
     	} else {
-    		return $this->current() !== FALSE;
+    		return $this->current() !== false;
     	}
     }
 
 	/**
 	 * Reset the internal pointer of the collection to the first item.
 	 */
-    public function rewind() {
-    	if ($this->__pageItems > 0) {
+    public function rewind()
+    {
+    	if ($this->pageItems > 0) {
 			$this->setCurrentPage();
     		$this->seek($this->pageStart() - 1);
     	} else {
@@ -260,7 +253,8 @@ class DBA__Collection implements Iterator {
 	/**
 	 * Reverse the order of the collection and return it.
 	 */
-    public function reverse() {
+    public function reverse()
+    {
 		$this->collection = array_reverse($this->collection);
         return $this;
     }
@@ -268,7 +262,8 @@ class DBA__Collection implements Iterator {
 	/**
 	 * Set the internal pointer of the collection to the last item and return it.
 	 */
-    public function end() {
+    public function end()
+    {
         return end($this->collection);
     }
 
@@ -277,7 +272,8 @@ class DBA__Collection implements Iterator {
      *
      * @param variable $varValue
      */
-    public function inCollection($varValue) {
+    public function inCollection($varValue)
+    {
 		$blnReturn = false;
     	foreach ($this->collection as $object) {
     		if ($object == $varValue) {
@@ -297,8 +293,9 @@ class DBA__Collection implements Iterator {
      *
      * @param integer $intValue
      */
-	public function setPageItems($intValue) {
-		$this->__pageItems = $intValue;
+	public function setPageItems($intValue)
+	{
+		$this->pageItems = $intValue;
 
 		$this->setCurrentPage();
 		$this->seek($this->pageStart() - 1);
@@ -307,8 +304,9 @@ class DBA__Collection implements Iterator {
 	/**
 	 * Get the number of items per page.
 	 */
-	public function getPageItems() {
-		return $this->__pageItems;
+	public function getPageItems()
+	{
+		return $this->pageItems;
 	}
 
 	/**
@@ -316,11 +314,12 @@ class DBA__Collection implements Iterator {
 	 *
 	 * @param integer $intValue
 	 */
-	public function setCurrentPage($intValue = NULL) {
+	public function setCurrentPage($intValue = null)
+	{
 		if (is_null($intValue)) {
 			$intPage = 1;
 			$strRewrite	= Request::get('rewrite');
-			if (!empty($strRewrite) && mb_strpos($strRewrite, "__page") !== FALSE) {
+			if (!empty($strRewrite) && mb_strpos($strRewrite, "__page") !== false) {
 				$strRewrite = rtrim($strRewrite, " \/");
 				$arrParams = explode("/", $strRewrite);
 				$intPage = array_pop($arrParams);
@@ -329,26 +328,31 @@ class DBA__Collection implements Iterator {
 				$intPage = Request::get("page", 1);
 			}
 
-			if ($intPage > $this->pageCount() || $intPage < 1) $intPage = 1;
-			$this->__currentPage = $intPage;
+			if ($intPage > $this->pageCount() || $intPage < 1) {
+			    $intPage = 1;
+			}
+
+			$this->currentPage = $intPage;
 		} else {
-			$this->__currentPage = $intValue;
+			$this->currentPage = $intValue;
 		}
 	}
 
 	/**
 	 * Get the current page number.
 	 */
-	public function getCurrentPage() {
-		return $this->__currentPage;
+	public function getCurrentPage()
+	{
+		return $this->currentPage;
 	}
 
 	/**
 	 * Get the number of pages for this collection.
 	 */
-	public function pageCount() {
-		if ($this->__pageItems > 0) {
-			$intReturn = ceil(count($this->collection) / $this->__pageItems);
+	public function pageCount()
+	{
+		if ($this->pageItems > 0) {
+			$intReturn = ceil(count($this->collection) / $this->pageItems);
 		} else {
 			$intReturn = 1;
 		}
@@ -359,16 +363,20 @@ class DBA__Collection implements Iterator {
 	/**
 	 * Get the number of the first item in the current page.
 	 */
-	public function pageStart() {
-		return ($this->getCurrentPage() * $this->__pageItems) - ($this->__pageItems - 1);
+	public function pageStart()
+	{
+		return ($this->getCurrentPage() * $this->pageItems) - ($this->pageItems - 1);
 	}
 
 	/**
 	 * Get the number of the last item in the current page.
 	 */
-	public function pageEnd() {
-		$intReturn = ($this->getCurrentPage() * $this->__pageItems);
-		if ($intReturn > count($this->collection)) $intReturn = count($this->collection);
+	public function pageEnd()
+	{
+		$intReturn = ($this->getCurrentPage() * $this->pageItems);
+		if ($intReturn > count($this->collection)) {
+		    $intReturn = count($this->collection);
+		}
 
 		return $intReturn;
 	}
@@ -376,7 +384,8 @@ class DBA__Collection implements Iterator {
 	/**
 	 * Get the page number of the next page.
 	 */
-	public function nextPage() {
+	public function nextPage()
+	{
 		$intReturn = ($this->getCurrentPage() + 1 < $this->pageCount()) ? $this->getCurrentPage() + 1 : $this->pageCount();
 
 		return $intReturn;
@@ -385,7 +394,8 @@ class DBA__Collection implements Iterator {
 	/**
 	 * Get the page number of the previous page.
 	 */
-	public function previousPage() {
+	public function previousPage()
+	{
 		$intReturn = ($this->getCurrentPage() - 1 > 0) ? ($this->getCurrentPage() - 1) : 1;
 
 		return $intReturn;
@@ -396,14 +406,15 @@ class DBA__Collection implements Iterator {
 	 *
 	 * @param object $objChild
 	 */
-	public function getPageByChild($objChild) {
+	public function getPageByChild($objChild)
+	{
 		$intReturn = 1;
 
 		$intId = (is_object($objChild)) ? $objChild->getId() : $objChild;
 		$intCount = 1;
 		foreach ($this->collection as $object) {
 			if ($object->getId() == $intId) {
-				$intReturn = ceil($intCount / $this->__pageItems);
+				$intReturn = ceil($intCount / $this->pageItems);
 			}
 
 			$intCount++;
@@ -420,7 +431,8 @@ class DBA__Collection implements Iterator {
 	 *
 	 * @param object $objChild
 	 */
-	public function seekByChild($objChild) {
+	public function seekByChild($objChild)
+	{
     	$intReturn = 0;
 		$intId = (is_object($objChild)) ? $objChild->getId() : $objChild;
 
@@ -434,10 +446,8 @@ class DBA__Collection implements Iterator {
 			$intCount++;
 		}
 
-		$this->isSeek = TRUE;
+		$this->isSeek = true;
 
 		return $intReturn;
 	}
 }
-
-?>

651038f243e667921da14983abdb5f58d973742f
Robin van Baalen <robin@cattlea.com>
2015-08-17 17:43:35 -0400

    fix(ElementField): corrected issue mention
    This commit corrects the issue mentioned in 4a7f5edf220fb69456843ff59cad25c531fe74fd
    
    Fixes #7

--- classes/PunchCMS/Client/ElementField.php
+++ classes/PunchCMS/Client/ElementField.php
@@ -8,7 +8,7 @@ use PunchCMS\TemplateField;
 use PunchCMS\ImageField;
 use PunchCMS\ImageValue;
 
-class ElementField extends \PunchCMS\DBAL\ElementField
+class ElementField extends \PunchCMS\DBAL\ElementField 
 {
     private $objField;
     public $name;
